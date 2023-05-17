d4c5ecbd9eca9563070af10e883e437b4b62c567
Alisha <fogle.alisha@gmail.com>
2023-05-17 11:10:04 -0500

    First commit of Skyrim-Construction project

---
+++ Skyrim-Construction/src/skyrim/construction/SkyrimConstruction.java
@@ -0,0 +1,20 @@
+package skyrim.construction;
+
+/**
+ * Class: SkyrimConstruction
+ * Package: skyrim.construction
+ * @author Alisha
+ * @since 
+ * @version
+ * 
+ */
+public class SkyrimConstruction {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
